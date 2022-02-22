f9b89c7d55d1af0060bc3dff1ca05e6f9cb5e22a
AD <adwaitg02@gmail.com>
2022-02-22 20:40:26 +0530

    NM

---
+++ BACKEND/WebScrapping/twitter_scrap.py
@@ -0,0 +1,11 @@
+# TODO : Open Twitter
+
+# TODO : Login Into Account Just to be safe
+
+# TODO : Insert #tags into search bar
+
+""" TODO : Get the tweets and keep them seperated
+           Try to get images from tweets    """
+
+# TODO : Store data in JSON format
+
