ff9dd895bcd001d58592a0489899b69df29f16b6
AD <adwaitg02@gmail.com>
2022-03-08 20:20:11 +0530

    NM

--- BACKEND/WebScrapping/Testing/test.py
+++ BACKEND/WebScrapping/Testing/test.py
@@ -1,3 +1,5 @@
+# Dunno What it does but it does
+# Oh yeah I just remembered it adds , to end of every line
 with open('..//xyx.json', 'r', encoding='utf-8') as istr:
     with open('..//abc.json', 'w', encoding='utf-8') as ostr:
         for line in istr:
@@ -6,11 +8,13 @@ with open('..//xyx.json', 'r', encoding='utf-8') as istr:
 
     istr.close()
 
+# Adds text to beginning of json
 with open("..//abc.json", "r+", encoding='utf-8') as file:
     file.write('{"data":[{"id":')
     file.seek(0)
     content = file.read()
 
+# removes extra ,
 buggy_name = open('..//abc.json', 'r', encoding='utf-8')
 name = buggy_name.read()
 newtext = name[:-2]
@@ -19,8 +23,7 @@ newFile.write(newtext)
 newFile.close()
 print(newtext)
 
-
+# Adds text to end of json
 file = open('..//abc.json', 'a', encoding='utf-8')
 file.write(']}')
 file.close()
-
